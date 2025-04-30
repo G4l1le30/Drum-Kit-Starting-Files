755dc233b4742a07444951a81752fd5afd9d940e
Joshua <145852592+G4l1le30@users.noreply.github.com>
2025-04-30 20:50:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function makeSound(event){
                 case "s":
                     var tom3=new Audio("sounds/tom-3.mp3");
                     tom3.play();
-                    document.getElementById("title").textContent = "adji puki";
+                    document.getElementById("title").textContent = "test";
                     break;
                 case "d":
                     var tom4=new Audio("sounds/tom-4.mp3");
